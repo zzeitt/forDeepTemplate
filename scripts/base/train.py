edebc2793bf02e700f16f3f816ad74eb4f74b7e8
zeit-lab627 <zei.t@qq.com>
2022-11-07 17:24:32 +0800

    update: improve structure

--- scripts/base/train.py
+++ scripts/base/train.py
@@ -3,96 +3,12 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 
-from scripts.data.monitor import MetricMonitor
-from scripts.train.checkpoint import CheckpointRestorer, CheckpointSaver
-from scripts.train.recorder import Backuper, TbRecorder, VisRecorder
+from scripts.base.data import MetricMonitor
+from scripts.base.checkpoint import CheckpointRestorer, CheckpointSaver
+from scripts.base.recorder import Backuper, TbRecorder, VisRecorder
 
 
 logger = logging.getLogger(__name__)
-
-
-class BaseTrainer:
-    def __init__(
-            self, lr, epochs, device, checkpoint, recorder, predict_only=False):
-        self.lr = lr
-        self.epochs = epochs
-        self.device = device
-        self.epoch_start = 1
-        self.predict_only = predict_only
-        if self.predict_only:
-            self.ckpt_restorer = make_ckpt_restorer(**checkpoint['restore'])
-            self.backuper = make_backuper(**recorder['backup'])
-            self.vis_recorder = make_vis_recorder(**recorder['vis'])
-            self.model = None
-            self.test_loader = None
-        else:
-            self.ckpt_saver = make_ckpt_saver(**checkpoint['save'])
-            self.ckpt_restorer = make_ckpt_restorer(**checkpoint['restore'])
-            self.backuper = make_backuper(**recorder['backup'])
-            self.tb_recorder = make_tb_recorder(**recorder['tb'])
-            self.vis_recorder = make_vis_recorder(**recorder['vis'])
-            self.model = None
-            self.optimizer = None
-            self.train_loader = None
-            self.val_loader = None
-            self.train_criterions_dict = {}
-            self.val_criterions_dict = {}
-
-    def train_per_epoch(self, epoch):
-        metric_monitor = MetricMonitor()
-        self.model.train()
-        stream = tqdm(self.train_loader)
-        for i, data in enumerate(stream, start=1):
-            images = data['image'].to(self.device, non_blocking=True)
-            target = data['target'].to(self.device, non_blocking=True)
-            output = self.model(images).squeeze(1)
-            ls_loss = []
-            for d in self.train_criterions_dict.values():
-                ls_loss.append(d['crit'](output, target)*d['weight'])
-            loss = sum(ls_loss)
-            metric_monitor.update("Loss", loss.item())
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-            stream.set_description(
-                f'Epoch: {epoch}. Train.      {metric_monitor}')
-            self.tb_recorder.record(
-                'loss/total', loss, (epoch-1)*len(self.train_loader)+(i-1))
-        logger.info(f'Epoch: {epoch}. Train.      {metric_monitor}')
-
-    def validate_per_epoch(self, epoch):
-        metric_monitor = MetricMonitor()
-        self.model.eval()
-        stream = tqdm(self.val_loader)
-        with torch.no_grad():
-            for i, data in enumerate(stream, start=1):
-                images = data['image'].to(self.device, non_blocking=True)
-                target = data['target'].to(self.device, non_blocking=True)
-                image_filenames = data['image_filename']
-                output = self.model(images).squeeze(1)
-                ls_loss = []
-                for d in self.val_criterions_dict.values():
-                    ls_loss.append(d['crit'](output, target)*d['weight'])
-                loss = sum(ls_loss)
-                metric_monitor.update("Loss", loss.item())
-                stream.set_description(
-                    f'Epoch: {epoch}. Validation. {metric_monitor}')
-                self.vis_recorder.record(
-                    images, image_filenames, epoch, 'src', 'normalized')
-                self.vis_recorder.record(
-                    target, image_filenames, epoch, 'src', 'target')
-                self.vis_recorder.record(
-                    output, image_filenames, epoch, 'res', 'output')
-                # todo: 'images' are transformed, maybe visrecord in prediction?
-        logger.info(f'Epoch: {epoch}. Validation. {metric_monitor}')
-
-    def train_and_val(self):
-        for epoch in range(self.epoch_start, self.epochs + 1):
-            self.train_per_epoch(epoch)
-            self.validate_per_epoch(epoch)
-            self.ckpt_saver.save('model', self.model, epoch)
-            self.ckpt_saver.save('optimizer', self.optimizer, epoch)
-        logger.info(f'Training finished.')
 
 
 def make_criterion(kind, **kwargs):
@@ -163,3 +79,87 @@ def make_vis_recorder(kind='base', **kwargs):
     else:
         raise ValueError(f'Unknown vis-recorder kind {kind}')
     return vis_recorder_class(**kwargs)
+
+
+class BaseTrainer:
+    def __init__(
+            self, lr, epochs, device, checkpoint, recorder, predict_only=False):
+        self.lr = lr
+        self.epochs = epochs
+        self.device = device
+        self.epoch_start = 1
+        self.predict_only = predict_only
+        if self.predict_only:
+            self.ckpt_restorer = make_ckpt_restorer(**checkpoint['restore'])
+            self.backuper = make_backuper(**recorder['backup'])
+            self.vis_recorder = make_vis_recorder(**recorder['vis'])
+            self.model = nn.Identity()
+            self.test_loader = None
+        else:
+            self.ckpt_saver = make_ckpt_saver(**checkpoint['save'])
+            self.ckpt_restorer = make_ckpt_restorer(**checkpoint['restore'])
+            self.backuper = make_backuper(**recorder['backup'])
+            self.tb_recorder = make_tb_recorder(**recorder['tb'])
+            self.vis_recorder = make_vis_recorder(**recorder['vis'])
+            self.model = nn.Identity()
+            self.optimizer = None
+            self.train_loader = None
+            self.val_loader = None
+            self.train_criterions_dict = {}
+            self.val_criterions_dict = {}
+
+    def train_per_epoch(self, epoch):
+        metric_monitor = MetricMonitor()
+        self.model.train()
+        stream = tqdm(self.train_loader)
+        for i, data in enumerate(stream, start=1):
+            images = data['image'].to(self.device, non_blocking=True)
+            target = data['target'].to(self.device, non_blocking=True)
+            output = self.model(images).squeeze(1)
+            ls_loss = []
+            for d in self.train_criterions_dict.values():
+                ls_loss.append(d['crit'](output, target)*d['weight'])
+            loss = sum(ls_loss)
+            metric_monitor.update("Loss", loss.item())
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            stream.set_description(
+                f'Epoch: {epoch}. Train.      {metric_monitor}')
+            self.tb_recorder.record(
+                'loss/total', loss, (epoch-1)*len(self.train_loader)+(i-1))
+        logger.info(f'Epoch: {epoch}. Train.      {metric_monitor}')
+
+    def validate_per_epoch(self, epoch):
+        metric_monitor = MetricMonitor()
+        self.model.eval()
+        stream = tqdm(self.val_loader)
+        with torch.no_grad():
+            for i, data in enumerate(stream, start=1):
+                images = data['image'].to(self.device, non_blocking=True)
+                target = data['target'].to(self.device, non_blocking=True)
+                image_filenames = data['image_filename']
+                output = self.model(images).squeeze(1)
+                ls_loss = []
+                for d in self.val_criterions_dict.values():
+                    ls_loss.append(d['crit'](output, target)*d['weight'])
+                loss = sum(ls_loss)
+                metric_monitor.update("Loss", loss.item())
+                stream.set_description(
+                    f'Epoch: {epoch}. Validation. {metric_monitor}')
+                self.vis_recorder.record(
+                    images, image_filenames, epoch, 'src', 'normalized')
+                self.vis_recorder.record(
+                    target, image_filenames, epoch, 'src', 'target')
+                self.vis_recorder.record(
+                    output, image_filenames, epoch, 'res', 'output')
+                # todo: 'images' are transformed, maybe visrecord in prediction?
+        logger.info(f'Epoch: {epoch}. Validation. {metric_monitor}')
+
+    def train_and_val(self):
+        for epoch in range(self.epoch_start, self.epochs + 1):
+            self.train_per_epoch(epoch)
+            self.validate_per_epoch(epoch)
+            self.ckpt_saver.save('model', self.model, epoch)
+            self.ckpt_saver.save('optimizer', self.optimizer, epoch)
+        logger.info(f'Training finished.')
